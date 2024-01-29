8e813af66ca7c2fbf8933b8dbf73b142da2029b8
devanshi00 <117909426+devanshi00@users.noreply.github.com>
2024-01-29 23:50:56 +0530

    Update main_lux-ai-2021.py

--- kaggle_submissions/main_lux-ai-2021.py
+++ kaggle_submissions/main_lux-ai-2021.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO  # pip install stable-baselines3
+from stable_baselines3 import A2C  # pip install stable-baselines3
 
 from luxai2021.env.agent import AgentFromStdInOut
 from luxai2021.env.lux_env import LuxEnvironment
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     #model_id = 5403
     #total_steps = int(48e6)
     #model = PPO.load(f"models/rl_model_{model_id}_{total_steps}_steps.zip")
-    model = PPO.load(f"model.zip")
+    model = A2C.load("rl_model_1_7000000_steps.zip")
     
     # Create a kaggle-remote opponent agent
     opponent = AgentFromStdInOut()
